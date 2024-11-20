b6c1a45d736cb1305322b411b5a42f4a9311b3db
Pachu03 <franfervaz03@gmail.com>
2024-11-20 09:45:59 +0100

    Creacion de las dos promesas y clase

--- Car.js
+++ Car.js
@@ -1,55 +1,52 @@
-class Car {
-  #id;
-  year;
-  make;
-  model;
-  type;
-
-  constructor(id, make) {
-    this.#id = id;
-    this.year = year;
-    this.make = make;
-    this.model = model;
-    this.type = type;
+export class Car {
+    #id;
+  
+    constructor(id, year, make, model, type) {
+      this.#id = id;
+      this._year = year; 
+      this._make = make;
+      this._model = model;
+      this._type = type;
+    }
+  
+    get id() {
+      return this.#id;
+    }
+  
+    set id(value) {
+      this.#id = value;
+    }
+  
+    get year() {
+      return this._year;
+    }
+  
+    set year(value) {
+      this._year = value;
+    }
+  
+    get make() {
+      return this._make;
+    }
+  
+    set make(value) {
+      this._make = value;
+    }
+  
+    get model() {
+      return this._model;
+    }
+  
+    set model(value) {
+      this._model = value;
+    }
+  
+    get type() {
+      return this._type;
+    }
+  
+    set type(value) {
+      this._type = value;
+    }
   }
-
-  get id() {
-    return this.#id;
-  }
-
-  set id(value) {
-    this.#id = value;
-  }
-
-  get year() {
-    return this.year;
-  }
-
-  set year(value) {
-    this.year = value;
-  }
-
-  get make() {
-    return this.make;
-  }
-
-  set make(value) {
-    this.make = value;
-  }
-
-  get model() {
-    return this.model;
-  }
-
-  set model(value) {
-    this.model = value;
-  }
-
-  get type() {
-    return this.type;
-  }
-
-  set type(value) {
-    this.type = value;
-  }
-}
+  
--- main.js
+++ main.js
@@ -1,7 +1,49 @@
 import "./style.css";
+import { Car } from "./Car.js";
+import cars from "./mockData.js";
 
-const iniciarEventos = () => {
-  
+const iniciarEvento = () => {
+  mapToCarClasses(cars)
+    .then((carInstances) => {
+      console.log("Instancias de Car:");
+      console.log(carInstances);
+
+      filterCar(carInstances)
+        .then((carFilters) => {
+          console.log(carFilters);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 };
 
-document.body.addEventListener("DOMContentLoaded", iniciarEventos);
+const mapToCarClasses = (carsData) => {
+  return new Promise((resolve, reject) => {
+    const carInstances = carsData.map(
+      (car) => new Car(car.id, car.year, car.make, car.model, car.type)
+    );
+    if (carInstances) {
+      resolve(carInstances);
+    } else {
+      reject("Error al mapear los datos");
+    }
+  });
+};
+
+const filterCar = (cars) => {
+  return new Promise((resolve, reject) => {
+    const carsFilters = cars.filter((car) => car.year > 2010);
+
+    if (carsFilters) {
+      resolve(carsFilters);
+    } else {
+      reject("Error al filtrar los datos");
+    }
+  });
+};
+
+document.addEventListener("DOMContentLoaded", iniciarEvento);
